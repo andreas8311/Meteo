db2647d870a3df99774a2a2e7092119e542472a7
Andreas Jakobsson <Andreas8311@hotmail.com>
2022-03-11 10:54:00 +0100

    Adjusting color scheme on prediction

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ if st.button('Predict Weather'):
     for i in range(10):
         one_frame = new_predictions[i]
 
-        one_frame = np.where(one_frame < 0.02, 0, one_frame)  # one_frame*2.5
+        one_frame = np.where(one_frame < 0.02, 0, one_frame*3)  # one_frame*2.5
         # print("fra2 shape : ",one_frame.shape)
         new_predictions2[i] = one_frame
 
